9d234a26b3d0cefd6df33a9299f2d18e2eec6119
Gustavo Silva <gustavosilv4.contato@gmail.com>
2023-12-23 19:41:57 -0300

    feat: create component sidebar

--- ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/App.jsx
+++ ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,26 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 
+import styles from './App.module.css'
+
 import "./global.css"
+import Sidebar from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author='Gustavo Miller'
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, doloribus! Nihil neque eos dolorem reprehenderit magnam. Sequi impedit, quam tempora voluptas veniam totam minus expedita blanditiis, harum, labore minima. Mollitia!"
-      />
-      <Post author='Fernanda Oliveira' content='Ola tudo bem?' />
-      <Post />
-      <Post />
-      <Post />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Gustavo Miller'
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, doloribus! Nihil neque eos dolorem reprehenderit magnam. Sequi impedit, quam tempora voluptas veniam totam minus expedita blanditiis, harum, labore minima. Mollitia!"
+          />
+          <Post author='Fernanda Oliveira' content='Ola tudo bem?' />
+        </main>
+      </div>
     </div>
   )
 }
