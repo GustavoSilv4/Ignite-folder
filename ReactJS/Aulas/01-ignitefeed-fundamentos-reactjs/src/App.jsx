ca1ae37d6aed859e14d615b38f0e9ce10caba800
Gustavo Silva <gustavosilv4.contato@gmail.com>
2023-12-23 18:59:49 -0300

    feat: create global css

--- ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/App.jsx
+++ ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,20 @@
-function App() {
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+import "./global.css"
+
+export function App() {
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Header />
+      <Post
+        author='Gustavo Miller'
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, doloribus! Nihil neque eos dolorem reprehenderit magnam. Sequi impedit, quam tempora voluptas veniam totam minus expedita blanditiis, harum, labore minima. Mollitia!"
+      />
+      <Post author='Fernanda Oliveira' content='Ola tudo bem?' />
+      <Post />
+      <Post />
+      <Post />
+    </div>
   )
 }
-
-export default App
