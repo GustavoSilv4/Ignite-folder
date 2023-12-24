32a2b4a76a184357e8d0325824462ef319cd5d18
Gustavo Silva <gustavosilv4.contato@gmail.com>
2023-12-24 19:33:11 -0300

    feat: render dynamically posts and comments

--- ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/App.jsx
+++ ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,46 @@ import styles from './App.module.css'
 import "./global.css"
 import Sidebar from "./components/Sidebar"
 
+// author: {avatar_url: '', name: '', role: ''},
+// publishedAt: Date,
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gustavosilv4.png',
+      name: 'Gustavo Silva',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-12-24 17:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-12-24 17:00:00')
+  },
+
+]
+
 export function App() {
   return (
     <div>
@@ -14,11 +54,18 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author='Gustavo Miller'
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, doloribus! Nihil neque eos dolorem reprehenderit magnam. Sequi impedit, quam tempora voluptas veniam totam minus expedita blanditiis, harum, labore minima. Mollitia!"
-          />
-          <Post author='Fernanda Oliveira' content='Ola tudo bem?' />
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  key={post.id}
+                  author={post.author}
+                  publishedAt={post.publishedAt}
+                  content={post.content}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </div>
--- ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/components/Post.jsx
+++ ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/components/Post.jsx
@@ -1,37 +1,69 @@
+import { useState } from 'react'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const [comments, setComments] = useState([
+    'Muito bom Devon, parabéns!! 👏👏',
+  ])
+  const [inputNewComment, setInputNewComment] = useState('')
+
+  const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", { locale: ptBR })
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
+  function handleCreateNewComment(e) {
+    e.preventDefault()
+
+    if (inputNewComment !== '') {
+      setComments(state => [...state, inputNewComment])
+      setInputNewComment('')
+    }
+
+    return
+
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar url={'https://github.com/diego3g.png'} />
+          <Avatar url={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Gustavo Silva</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="12 de dezembro às 08:21h" dateTime='2023-12-23 08:21:21'>Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p><a href="#">👉 jane.design/doctorcare</a></p>
-
-        <p><a href="#"> #novoprojeto #nlw #rocketseat</a></p>
+        {
+          content.map(line => {
+            if (line.type === 'paragraph') {
+              return <p key={line.content}>{line.content}</p>
+            } else if (line.type === 'link') {
+              return <p key={line.content}><a href="#">{line.content}</a></p>
+            }
+          })
+        }
 
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          value={inputNewComment}
+          onChange={(e) => setInputNewComment(e.target.value)}
           placeholder='Deixe um comentário'
         />
 
@@ -42,9 +74,11 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {
+          comments.map(comment => {
+            return <Comment key={comment} content={comment} />
+          })
+        }
       </div>
     </article>
   )
