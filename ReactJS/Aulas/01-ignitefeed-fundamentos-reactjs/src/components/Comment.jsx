6d51c8595d3cbefb884e8239db3ca0f1f9688ab0
Gustavo Silva <gustavosilv4.contato@gmail.com>
2023-12-24 22:49:20 -0300

    feat: create function to add likes

--- ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/components/Comment.jsx
+++ ReactJS/Aulas/01-ignitefeed-fundamentos-reactjs/src/components/Comment.jsx
@@ -3,10 +3,17 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 
 import styles from './Comment.module.css'
+import { useState } from 'react'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(state => state + 1)
   }
 
   return (
@@ -30,8 +37,8 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
-            <ThumbsUp /> Aplaudir <span>20</span>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp /> Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
