cc4a90aa997885b0050177dc706d617a2d9f2bbc
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-03 15:22:04 -0300

    Estrutura de uma aplicação em nodejs

--- NodeJS/Aulas/01-fundamentos-nodejs/src/server.js
+++ NodeJS/Aulas/01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,28 @@
 import http from 'node:http'
 
+const users = [] // Stateless - Dados salvo em memoria, onde é perdido a cada restart da aplicação / Statefull - Dados são salvos de forma externa sem perda
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello Gustavo!');
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: "John.Doe@gmail.com"
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
