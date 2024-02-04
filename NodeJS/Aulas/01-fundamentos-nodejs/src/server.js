64d917614402c5d104da6548b5d5dbede43bfe4b
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-04 18:21:55 -0300

    Capturando query parameters

--- NodeJS/Aulas/01-fundamentos-nodejs/src/server.js
+++ NodeJS/Aulas/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 // const users = [] // Stateless - Dados salvo em memoria, onde é perdido a cada restart da aplicação / Statefull - Dados são salvos de forma externa sem perda
 
 const server = http.createServer(async (req, res) => {
@@ -13,7 +14,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path)
 
-    req.params = { ...routeParams.groups }
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
     return route.handler(req, res)
   }
