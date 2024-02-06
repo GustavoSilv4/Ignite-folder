22adc4eacacd77392f4ce5757b161fbff583b06a
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-05 21:35:36 -0300

    Validando a existencia dos cookies com preHandlers/Middlewares por rotas

--- NodeJS/Aulas/02-api-rest-nodejs/src/routes/transactions.ts
+++ NodeJS/Aulas/02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,29 +1,41 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const sessionId = req.cookies.sessionId
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
 
     return { transactions }
   })
 
-  app.get('/:id', async (req) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (req) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
+    const sessionId = req.cookies.sessionId
+
     const { id } = getTransactionParamsSchema.parse(req.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex('transactions')
+      .where({ session_id: sessionId, id })
+      .first()
 
     return { transaction }
   })
 
-  app.get('/summary', async () => {
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const sessionId = req.cookies.sessionId
+
     const summary = await knex('transactions')
       .sum('amount', { as: 'amount' })
+      .where('session_id', sessionId)
       .first()
 
     return { summary }
