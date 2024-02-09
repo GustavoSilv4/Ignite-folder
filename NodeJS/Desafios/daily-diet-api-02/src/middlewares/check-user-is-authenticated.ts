502509f5ffd8c1ad34763ca922eb0b61919cfa8b
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-08 23:34:55 -0300

    feat: create middleware to verify if user is logged

---
+++ NodeJS/Desafios/daily-diet-api-02/src/middlewares/check-user-is-authenticated.ts
@@ -0,0 +1,13 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function checkUserIsAuthenticated(
+  req: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const sessionId = req.cookies.sessionId
+  console.log(req.cookies.sessionId)
+
+  if (!sessionId) {
+    return reply.status(401).send({ error: 'Not authenticated' })
+  }
+}
