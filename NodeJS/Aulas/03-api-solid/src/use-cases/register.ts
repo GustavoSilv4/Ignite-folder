88bfa3ac3ea75ba54987e33bb3bf7849140e5516
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-11 16:47:52 -0300

    Primeiro teste unitario

--- NodeJS/Aulas/03-api-solid/src/use-cases/register.ts
+++ NodeJS/Aulas/03-api-solid/src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exist-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,11 +9,19 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface UserUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ email, name, password }: RegisterUseCaseRequest) {
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterUseCaseRequest): Promise<UserUseCaseResponse> {
     const password_hash = await bcrypt.hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,6 +30,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({ name, email, password_hash })
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return {
+      user,
+    }
   }
 }
