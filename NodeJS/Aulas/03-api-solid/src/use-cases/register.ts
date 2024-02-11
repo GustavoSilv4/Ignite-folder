2b5fbf887dd363dffe09d74b65ae84832da7635b
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-11 15:19:56 -0300

    Criando interface para repositorio

--- NodeJS/Aulas/03-api-solid/src/use-cases/register.ts
+++ NodeJS/Aulas/03-api-solid/src/use-cases/register.ts
@@ -1,5 +1,5 @@
 import { hash } from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,16 +8,13 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(usersRepository: any) {}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ email, name, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('Email already exists.')
