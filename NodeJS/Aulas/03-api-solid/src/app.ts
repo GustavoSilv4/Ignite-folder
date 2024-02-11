102f6b1d078cf32bf665ff9afdf048109243cf0c
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-10 23:20:27 -0300

    Criado o docker compose

--- NodeJS/Aulas/03-api-solid/src/app.ts
+++ NodeJS/Aulas/03-api-solid/src/app.ts
@@ -1,13 +1,3 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    email: 'dasd',
-    name: 'dasd',
-  },
-})
