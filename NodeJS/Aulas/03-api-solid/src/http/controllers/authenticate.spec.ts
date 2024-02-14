cd376c36b4b129d54e2bd768b97e3a506a7c94f3
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-13 23:34:37 -0300

    Criado teste e2e para controller authenticate

--- NodeJS/Aulas/03-api-solid/src/http/controllers/authenticate.spec.ts
+++ NodeJS/Aulas/03-api-solid/src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import req from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Authenticate (e2e)', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await req(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123456',
+    })
+
+    const response = await req(app.server).post('/sessions').send({
+      email: 'john@doe.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
