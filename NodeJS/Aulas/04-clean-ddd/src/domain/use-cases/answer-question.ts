ae0f949b41e2f29e15b0614e640371739bb46d18
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-23 21:55:22 -0300

    Mais abstracoes de entidades

--- NodeJS/Aulas/04-clean-ddd/src/domain/use-cases/answer-question.ts
+++ NodeJS/Aulas/04-clean-ddd/src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswerRepository } from "../repositories/answers-repository";
 
@@ -15,7 +16,11 @@ export class AnswerQuestionUseCase {
     questionId,
     content,
   }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({ content, questionId, authorId: instructorId });
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    });
 
     await this.answersRepository.create(answer);
 
