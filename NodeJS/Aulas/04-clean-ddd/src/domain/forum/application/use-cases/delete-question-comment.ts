6e364c03d7ebca4065420b285c7df04f7f94067e
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-25 14:49:54 -0300

    Use Case: Deletar comentario de uma question

---
+++ NodeJS/Aulas/04-clean-ddd/src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -0,0 +1,30 @@
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface DeleteQuestionCommentUseCaseRequest {
+  authorId: string
+  questionCommentId: string
+}
+
+interface DeleteQuestionCommentUseCaseResponse {}
+
+export class DeleteQuestionCommentUseCase {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+
+  async execute({
+    authorId,
+    questionCommentId,
+  }: DeleteQuestionCommentUseCaseRequest): Promise<DeleteQuestionCommentUseCaseResponse> {
+    const questionComment =
+      await this.questionCommentsRepository.findById(questionCommentId)
+
+    if (!questionComment) throw new Error('Question comment not found.')
+
+    if (questionComment.authorId.toString() !== authorId)
+      throw new Error('Not Allowed')
+
+    await this.questionCommentsRepository.delete(questionComment)
+
+    return {}
+  }
+}
