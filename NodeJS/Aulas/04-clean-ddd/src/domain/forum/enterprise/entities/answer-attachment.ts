d851650f34fa76a2dc549cdbf07de029dc180d6a
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-02-25 18:24:00 -0300

    Criando entidades de anexo

---
+++ NodeJS/Aulas/04-clean-ddd/src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -0,0 +1,23 @@
+import { Entity } from '@/domain/core/entities/entity'
+import { UniqueEntityID } from '@/domain/core/entities/unique-entity-id'
+
+interface AnswerAttachmentProps {
+  answerId: UniqueEntityID
+  attachmentId: UniqueEntityID
+}
+
+export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
+  get answerId() {
+    return this.props.answerId
+  }
+
+  get attachmentId() {
+    return this.props.attachmentId
+  }
+
+  static create(props: AnswerAttachmentProps, id?: UniqueEntityID) {
+    const answerAttachment = new AnswerAttachment(props, id)
+
+    return answerAttachment
+  }
+}
