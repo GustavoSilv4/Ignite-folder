52b935c05cc62a08322b35748309aef9f5cb3e53
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-03-02 20:41:37 -0300

    Criando Service para Prisma

--- NodeJS/Aulas/05-nest-clean/src/app.controller.ts
+++ NodeJS/Aulas/05-nest-clean/src/app.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get('/hello')
-  handler(): string {
-    return 'Ola gustavo'
+  handler() {
+    return this.prisma.user.findMany()
   }
 
   @Post('/hello')
