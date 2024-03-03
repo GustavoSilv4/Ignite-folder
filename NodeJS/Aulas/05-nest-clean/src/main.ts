5af8136db9dd32972354c6665692fcd635377b67
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-03-03 14:07:07 -0300

    Criando primeiro Controller de criacao de conta

--- NodeJS/Aulas/05-nest-clean/src/main.ts
+++ NodeJS/Aulas/05-nest-clean/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    // logger: false, // Desabilitar o console de logs do nest
+  })
   await app.listen(3333)
 }
 bootstrap()
