250d9737f69eec87fc15a9ba4d660b2eacc3733b
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-03-10 14:55:41 -0300

    Implementado todos os repositorios Prisma Questions

--- NodeJS/Aulas/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ NodeJS/Aulas/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,5 +1,5 @@
 import { Question } from 'src/domain/forum/enterprise/entities/question'
-import { Question as PrismaQuestion } from '@prisma/client'
+import { Prisma, Question as PrismaQuestion } from '@prisma/client'
 import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
 import { Slug } from 'src/domain/forum/enterprise/entities/value-objects/slug'
 
@@ -19,5 +19,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toValue(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
   }
 }
