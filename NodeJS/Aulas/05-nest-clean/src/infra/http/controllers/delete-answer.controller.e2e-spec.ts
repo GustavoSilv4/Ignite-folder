4ef44e0dd8deff2387c5cd72a10747b9e1bd95b4
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-03-17 16:21:35 -0300

    Criando controller Deletar  uma Answer

---
+++ NodeJS/Aulas/05-nest-clean/src/infra/http/controllers/delete-answer.controller.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { PrismaService } from '../../database/prisma/prisma.service'
+import { AppModule } from '../../app.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { JwtService } from '@nestjs/jwt'
+import { DatabaseModule } from 'src/infra/database/database.module'
+import { StudentFactory } from 'src/test/factories/make-student'
+import { QuestionFactory } from 'src/test/factories/make-question'
+import { AnswerFactory } from 'src/test/factories/make-answer'
+
+describe('Delete Answers (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[Delete] /answers/:id', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaQuestion({
+      authorId: user.id,
+      questionId: question.id,
+    })
+
+    const answerId = answer.id.toString()
+
+    const response = await request(app.getHttpServer())
+      .delete(`/answers/${answerId}`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send()
+
+    expect(response.statusCode).toBe(204)
+
+    const answerOnDatabase = await prisma.answer.findUnique({
+      where: {
+        id: answerId,
+      },
+    })
+
+    expect(answerOnDatabase).toBeNull()
+  })
+})
--- NodeJS/Aulas/05-nest-clean/src/infra/http/http.module.ts
+++ NodeJS/Aulas/05-nest-clean/src/infra/http/http.module.ts
@@ -19,6 +19,8 @@ import { AnswerQuestionController } from './controllers/answer-question.controll
 import { AnswerQuestionUseCase } from 'src/domain/forum/application/use-cases/answer-question'
 import { EditAnswerController } from './controllers/edit-answer.controller'
 import { EditAnswerUseCase } from 'src/domain/forum/application/use-cases/edit-answer'
+import { DeleteAnswerController } from './controllers/delete-answer.controller'
+import { DeleteAnswerUseCase } from 'src/domain/forum/application/use-cases/delete-answer'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -32,6 +34,7 @@ import { EditAnswerUseCase } from 'src/domain/forum/application/use-cases/edit-a
     DeleteQuestionController,
     AnswerQuestionController,
     EditAnswerController,
+    DeleteAnswerController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -43,6 +46,7 @@ import { EditAnswerUseCase } from 'src/domain/forum/application/use-cases/edit-a
     DeleteQuestionUseCase,
     AnswerQuestionUseCase,
     EditAnswerUseCase,
+    DeleteAnswerUseCase,
   ],
 })
 export class HttpModule {}
